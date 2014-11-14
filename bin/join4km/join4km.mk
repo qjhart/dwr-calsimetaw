d98635847ada17c30576ea2f85607731dbea7564
Quinn Hart <qjhart@ucdavis.edu>
2014-11-14 02:26:21 +0000

    Added Simetaw output format

--- bin/join4km/join4km.mk
+++ bin/join4km/join4km.mk
@@ -50,6 +50,17 @@ join4km.daily:
 	 done ; \
 	done
 
+join4km.simetaw: 
+	[[ -d ${out}/joinkm ]] || mkdir -p ${out}/join4km;\
+	cd ${out}/join4km;\
+	for y in `seq 0 265`; do \
+	 row=`printf "%03d" $$y`; \
+	 echo $$row;\
+	 for x in `${PG} -t -c "select distinct x from join4km.daily$${row} order by x"`; do \
+	  ${PG} -q -t -c "\COPY (select extract(year from ymd) as year,extract(month from ymd) as month,extract(doy from ymd) as doy,lat::decimal(6,2),lon::decimal(6,2),Tx::decimal(6,2) as Tmax,Tn::decimal(6,2) as Tmin,pcp::decimal(6,2),eto::decimal(6,2)  from join4km.daily$${row} join \"4km\".pixels using (x,y) where x=$${x} order by ymd) to G$${y}_$${x}DPRISMETo_2.csv with csv header"; \
+	 done ; \
+	done
+
 join4km.daily.zip:
 	cd ${out}/join4km;\
 	for z in `ls daily_*.csv | cut -d_ -f 1,2 | sed -e 's/.$$//' | sort -u`; do \
